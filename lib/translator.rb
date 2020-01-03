395b055094b5d0c2b8537ad52ce6581a44dd5295
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 16:33:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
 require 'yaml'
 
-def load_library(library)
+def load_library(emoticon_file)
   # code goes here
   emoticons = YAML.load_file('./lib/emoticons.yml')
   
@@ -18,9 +18,9 @@ def load_library(library)
   return library
 end
 
-def get_japanese_emoticon(library, emoticon)
+def get_japanese_emoticon(emoticon_file, emoticon)
   # code goes here
-  initalize = load_library(library)
+  initalize = load_library(emoticon_file)
   english = initalize['get_meaning'][emoticon]
   english ? english : "Sorry, that emoticon was not found"
 end
