db95b2843e67eed66ecfbc0bcd488ffee82f844b
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 16:10:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library(arguement)
   
   emoticons.each { |meaning, value|
     library['meaning'][value][0] = 
-    library['emoticon'][value][1] = 
+    library['emoticon'][value][1] = japa
   }
   
   
