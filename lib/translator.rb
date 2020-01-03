a528588c4923ba8364d792a6558655f41de523ec
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 16:53:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,11 +21,11 @@ end
 def get_japanese_emoticon(library, emoticon)
   # code goes here
   japanese = (load_library(library))['get_meaning'][emoticon]
-  japanese ? japanese : 'Sorry, that emoticon was not found'
+  #japanese : 'Sorry, that emoticon was not found'
 end
 
 def get_english_meaning(library, emoticon)
   # code goes here
   english = (load_library(library))['get_meaning'][emoticon]
-  english ? english : 'Sorry, that emoticon was not found'
+  #english ? english : 'Sorry, that emoticon was not found'
 end
